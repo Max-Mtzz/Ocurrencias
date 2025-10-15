1ba0a44db4ffd5378385e41460e2bf90baad813b
Max-Mtzz <20243ds119@utez.edu.mx>
2025-10-15 08:48:05 -0600

    Ejercicio Ocurrencias

---
+++ src/App.java
@@ -0,0 +1,14 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        SinglyLinkedList list = new SinglyLinkedList();
+
+        list.add(1);
+        list.add(2);
+        list.add(1);
+        list.add(3);
+        list.add(4);
+
+        list.print();
+        System.out.println("El numero 1 se repite: "+list.Ocurrencias(1)+" veces.");
+    }
+}
---
+++ src/SinglyLinkedList.java
@@ -0,0 +1,52 @@
+public class SinglyLinkedList {
+    //Instanciamos nuestra cabecera
+    private Node head;
+
+    //Creamos nuestro método para añadir
+    public void add(int data){
+        //Creamos un nodo
+        Node newNode = new Node(data);
+        //Verificamos si la cabeza esta vacia
+        if(head == null){
+            //Si la lista esta vacía el nuevo nodo se convierte en la cabeza
+            head = newNode;
+            return;
+        }
+        //En caso de no estar vacía va a reccorrer todos los nodos hasta encontrar la referencia(next) en null
+        //Comenzamos desde el primero
+        Node current = head;
+        while(current.next!=null){
+            current = current.next;
+        }
+        //Se agrega el nuevo nodo en la referencia del ultimo nodo
+        current.next = new Node(data);
+    }
+
+    //Imprimir datos
+    public void print(){
+        //El current equivale a la cabeza
+        Node current = head;
+        //Mientras el current sea diferente de null imprime el dato
+        while(current!=null){
+            System.out.println(current.data);
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    //Contar Ocurrencias
+    public int Ocurrencias(int data){
+        int ocurrencias = 0;
+        Node current = head;
+
+        while (current!=null) {
+            //Verifica si se repite un dato y aumenta las repeticiones
+            if (current.data == data) {
+                ocurrencias++;
+            }
+            current = current.next;
+        }
+
+        return ocurrencias;
+    }
+}
